c3dab2b0e5ad541295d1eced8f4b818581d4b93f
fatekha <136146103+fatekha@users.noreply.github.com>
2023-06-10 12:56:50 +0700

    Update Algo_latihan1.py
    Menambahkan source code tentang penilaian KHS Mahasiswa

--- Algo_latihan1.py
+++ Algo_latihan1.py
@@ -1,1 +1,34 @@
-maaf belum diisi, nantikan acara selanjutnya 
+Seting Variabel
+nim = "20122073"
+namaPutri Fatekha "
+ = "2A"
+prodi = "Informatika "
+#Setting Variabel nilai
+bhs_indo =76
+bhs_ing  =70
+pd       =90
+mtk      =65
+kal1    =67
+so       =75
+
+# Perhitungan
+rata = (bhs_indo+bhs_ing+pd+mtk+kal1+so)/6
+
+# Cetak Hasil 
+print ("-------------------------")
+print ("NIM Anda            :",nim)
+print ("Nama Lengkap Anda  :",nama)
+print ("Kelas             :",kelas)
+print ("Program Studi     :",prodi)
+print ("-------------------------")
+print ("Hasil Kartu Hasil Studi")
+print ("-------------------------")
+print ("1. Bahas Indonesia ",bhs_indo)
+print ("1. Bahas Inggris ",bhs_ing)
+print ("2. Pemrograman Dasar",pd)
+print ("3. Matematika ",mtk)
+print ("4. kalkulus ",kal1)
+print ("5. Sistem Operasi  ",so)
+print ("-------------------------")
+print ("Rata-rata.      ",rata)
+print ("-------------------------")
