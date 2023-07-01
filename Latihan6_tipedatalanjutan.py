1f5bb45433c04717ebc906dce2945b38bca82888
fatekha <136146103+fatekha@users.noreply.github.com>
2023-07-01 21:48:01 +0700

    Update Latihan6_tipedatalanjutan.py

--- Latihan6_tipedatalanjutan.py
+++ Latihan6_tipedatalanjutan.py
@@ -1,1 +1,51 @@
+jari = int(input("Berapa jari-jari lingkaran: "))
+phi = float(input("Masukkan nilai phi: "))
 
+luas = phi * jari * jari
+keliling = 2*phi*jari
+
+print("----------------------------------------")
+print("      OPERASI ARITMATIKA      ")
+print("----------------------------------------")
+nim = 20122073
+nama = "PUTRI FATEKHA"
+kelas = 2
+identitas = "TI"
+print("nim: %d" % (nim)
+print("nama: %s" % nama)
+print("kelas: %d" % kelas)
+print("prodi: %s" % identitas)
+print("----------------------------------------")
+print("          LINGKARAN           ")
+print("----------------------------------------")
+print("Jari-jari lingkaran: %d" % jari)
+print("Nilai phi: %d" % phi)  
+print("Luas lingkaran: %d" % luas)  
+print("keliling lingkaran: %d" % keliling)
+
+list =["uniss", "di", "belajar", "pada", "mahasiswa", "pemrograman", "ruang", "lab", "algoritma", "semester", "topik", "data","tipe", "dan", "dengan", "Batang", 2]
+#foword
+print("----------------------------------------------------")
+print("         Urutan Kata Yang Benar         ")
+print("----------------------------------------------------")
+print(list[4]) 
+print(list[9]) 
+print(list[16]) 
+print(list[2])
+print(list[8])
+print(list[5])
+print(list[14])
+print(list[10])
+print(list[12])
+print(list[11])
+print(list[1]) 
+print(list[6])
+print(list[7])
+print(list[1])
+print(list[0])
+print(list[15])
+print("----------------------------------------------------")
+print("      menjadi rangkaian kalimat      ")
+print("----------------------------------------------------")
+#spok 
+print("Mahasiswa semester 2 belajar algoritma pemrograman dengan topik tipe data di ruang lab di Uniss Batang.")
