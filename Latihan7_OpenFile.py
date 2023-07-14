1192e97f9698aa7ac60d6250b48a2823f0d76828
fatekha <136146103+fatekha@users.noreply.github.com>
2023-07-14 21:38:47 +0700

    Update Latihan7_OpenFile.py

--- Latihan7_OpenFile.py
+++ Latihan7_OpenFile.py
@@ -1,1 +1,30 @@
+# Open File 
+data = open("data_iris.txt", "r")
+print ("Nama File",data.name)
 
+#read
+file_konten = data.read(100)
+print ("Isa Data:",file_konten[18:40])
+
+#Closed
+data.close()
+# For Loops
+#for i in range(1,10):
+        #print(1)
+list_dosen =("Mawar","selasa","Dedi","Lukman")
+list_jatwal =("senin","selasa","rabu","kamis","jumat")
+
+for i in (list_dosen):
+  for k in (list_jadwal):
+    print(i, " -" ,k)
+    
+    
+# Fibonanci
+n=10
+fib = [0,1]
+
+for i in range(n-2):
+  fib_lanjut = fib[i+1] + fib[i]
+  print(fib[i+1]," + ",fib[i]," = ",fib_lanjut)
+  fib.append(fib_lanjut)
+  print(fib)
